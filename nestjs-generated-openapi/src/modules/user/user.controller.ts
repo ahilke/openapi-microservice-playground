c6a259c4e45f6a4b7c3777a8902cb4babd40723b
Arno Hilke <arno.hilke@tum.de>
2022-02-20 21:48:09 +0100

    Fix OpenAPI Generation
    The generated OpenAPI was not actually using the Nest CLI or
    OpenAPI plugin for that matter. It also did not include a DTO
    to type the response schema.

--- nestjs-generated-openapi/src/modules/user/user.controller.ts
+++ nestjs-generated-openapi/src/modules/user/user.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Param } from "@nestjs/common";
+import { UserDto } from "./user.dto";
 
 @Controller()
 export class UserController {
     @Get("users/:id")
-    public getUser(@Param("id") _id: string) {
+    public getUser(@Param("id") _id: string): UserDto {
         return {
             id: "2dce19f8-40af-4ef3-9e44-519df4cfa157",
             firstName: "Terry",
